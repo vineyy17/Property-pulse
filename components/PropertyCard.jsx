eb82cef65dbfc446932c1318d24ba12a45e85832
Divine Charles <officialkb17@gmail.com>
2024-04-29 19:35:20 +0100

    Make property data dynamic

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -1,57 +1,96 @@
-const PropertyCard = () => {
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaMoneyBill,
+  FaMapMarker,
+} from 'react-icons/fa';
+
+const PropertyCard = ({ property }) => {
+  const getRateDisplay = () => {
+    const { rates } = property;
+
+    if (rates.monthly) {
+      return `${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `${rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nighly) {
+      return `${rates.nightly.toLocaleString()}/night`;
+    }
+  };
+
   return (
     <div class="rounded-xl shadow-md relative">
-      <img
-        src="images/properties/a1.jpg"
+      <Image
+        src={`/images/properties/${property.images[0]}`}
         alt=""
         class="w-full h-auto rounded-t-xl"
+        sizes="100vw"
+        height={0}
+        width={0}
       />
       <div class="p-4">
         <div class="text-left md:text-center lg:text-left mb-6">
-          <div class="text-gray-600">Apartment</div>
-          <h3 class="text-xl font-bold">Boston Commons Retreat</h3>
+          <div class="text-gray-600">{property.type}</div>
+          <h3 class="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 class="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
-          $4,200/mo
+          ${getRateDisplay()}
         </h3>
 
         <div class="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <i class="fa-solid fa-bed"></i> 3
+            <FaBed className="inline mr-2" /> {property.beds}{' '}
             <span class="md:hidden lg:inline">Beds</span>
           </p>
           <p>
-            <i class="fa-solid fa-bath"></i> 2
+            <FaBath className="inline mr-2" /> {property.baths}{' '}
             <span class="md:hidden lg:inline">Baths</span>
           </p>
           <p>
-            <i class="fa-solid fa-ruler-combined"></i>
-            1,500 <span class="md:hidden lg:inline">sqft</span>
+            <FaRulerCombined className="inline mr-2" />
+            {property.square_feet} <span class="md:hidden lg:inline">sqft</span>
           </p>
         </div>
 
         <div class="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          <p>
-            <i class="fa-solid fa-money-bill"></i> Weekly
-          </p>
-          <p>
-            <i class="fa-solid fa-money-bill"></i> Monthly
-          </p>
+          {property.rates.nightly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Nightly
+            </p>
+          )}
+
+          {property.rates.weekly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Weekly
+            </p>
+          )}
+
+          {property.rates.monthly && (
+            <p>
+              <FaMoneyBill className="inline mr-2" /> Monthly
+            </p>
+          )}
         </div>
 
         <div class="border border-gray-100 mb-5"></div>
 
         <div class="flex flex-col lg:flex-row justify-between mb-4">
           <div class="flex align-middle gap-2 mb-4 lg:mb-0">
-            <i class="fa-solid fa-location-dot text-lg text-orange-700"></i>
-            <span class="text-orange-700"> Boston MA </span>
+            <FaMapMarker className="text-orange-700 mt-1" />
+            <span class="text-orange-700">
+              {' '}
+              {property.location.city} {property.location.state}{' '}
+            </span>
           </div>
-          <a
-            href="property.html"
+          <Link
+            href={`/properties/${property._id}`}
             class="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
