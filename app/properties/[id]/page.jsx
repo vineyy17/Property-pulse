4f34eeba7ccab01588b0b6328357f28d879afb6b
Divine Charles <officialkb17@gmail.com>
2024-05-03 17:18:09 +0100

    Create Spinner component for loading state

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import Link from 'next/link';
 import PropertyDetails from '@/components/PropertyDetails';
 import { FaArrowLeft } from 'react-icons/fa';
+import Spinner from '@/components/Spinner';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -41,6 +42,7 @@ const PropertyPage = () => {
 
   return (
     <>
+      {loading && <Spinner loading={loading} />}
       {!loading && property && (
         <>
           <PropertyHeaderImage image={property.images[0]} />
