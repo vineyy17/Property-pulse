f1eb8b3defd37390176ea91e6c35bef166e20e29
Divine Charles <officialkb17@gmail.com>
2024-05-03 04:30:56 +0100

    Add requests file

---
+++ utils/requests.js
@@ -0,0 +1,21 @@
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+async function fetchProperties() {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${apiDomain}/properties`);
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export { fetchProperties };
