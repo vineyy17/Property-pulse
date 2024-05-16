0cb74361cdf69ecd54ee7332aec932054ea3775c
Divine Charles <officialkb17@gmail.com>
2024-05-16 14:25:59 +0100

    Implement display of messages

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Message from './Message';
+import Spinner from './Spinner';
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
@@ -25,7 +27,27 @@ const Messages = () => {
     getMessages();
   }, []);
 
-  return <div>Messages</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+
+          <div className="space-y-4">
+            {messages.length === 0 ? (
+              <p>You have no messages</p>
+            ) : (
+              messages.map((message) => (
+                <Message key={message._id} message={message} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
