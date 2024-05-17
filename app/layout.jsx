d0d62b53da2b26bb7c9d020f23eb8a289e8ccbab
Divine Charles <officialkb17@gmail.com>
2024-05-17 15:38:03 +0100

    Implement photoswipe lightbox

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import AuthProvider from '@/components/AuthProvider';
 import { GlobalProvider } from '@/context/GlobalContext';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
 import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({
