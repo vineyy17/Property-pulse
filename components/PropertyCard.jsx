8834493fb505d75663c89e56165116f04b7c79f7
Divine Charles <officialkb17@gmail.com>
2024-05-19 10:53:37 +0100

    Remove image object cover style

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -31,9 +31,7 @@ const PropertyCard = ({ property, isFeatured = false }) => {
         src={property.images[0]}
         alt=""
         className={`w-full h-auto rounded-t-xl ${
-          isFeatured
-            ? 'md:w-2/5 md:rounded-tr-none md:rounded-l-xl object-cover'
-            : ''
+          isFeatured ? 'md:w-2/5 md:rounded-tr-none md:rounded-l-xl' : ''
         }`}
         sizes="100vw"
         height={0}
