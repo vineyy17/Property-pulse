e878acd8806d52e8b0cb860aa6d120bb8bdb580f
Divine Charles <officialkb17@gmail.com>
2024-05-19 11:31:07 +0100

    Attempt prod fix

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -23,9 +23,11 @@ const PropertyCard = ({ property, isFeatured = false }) => {
 
   return (
     <div
-      className={`rounded-xl shadow-md relative ${
-        isFeatured ? 'bg-white flex flex-col md:flex-row' : ''
-      }`}
+      // className={`rounded-xl shadow-md relative ${
+      //   isFeatured ? 'bg-white flex flex-col md:flex-row' : ''
+      // }`}
+
+      className="rounded-xl shadow-md relative"
     >
       <Image
         src={property.images[0]}
@@ -44,17 +46,13 @@ const PropertyCard = ({ property, isFeatured = false }) => {
           <div className="text-gray-600">{property.type}</div>
           <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
-        {/* <h3
+        <h3
           className={`${
             !isFeatured ? 'absolute top-[10px] right-[10px]' : ''
           } bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right ${
             isFeatured ? 'absolute top-[10px] left-[10px]' : ''
           }`}
         >
-          ${getRateDisplay()}
-        </h3> */}
-
-        <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
           ${getRateDisplay()}
         </h3>
 
